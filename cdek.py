b64608a6d30fd79b595bc749fc812c25ea410df1
kkashicyn <kkashicyn@gmail.com>
2021-07-22 21:37:46 +0300

    исправления

--- cdek.py
+++ cdek.py
@@ -1,57 +1,66 @@
 import requests
+import time
 
 
 class Cdek:
-    
-    httpclient = object
+    http = object
 
     def __init__(self, account, secret_key):
-        self.httpclient = HttpClient(account=account, secret_key=secret_key)
-
-    def get_order(self, uuid_number):
-        return self.httpclient.request(url='orders/', method='get', data={'uuid_number': uuid_number})
+        self.http = HttpClient(account=account, secret_key=secret_key)
 
     def new_order(self, data):
-        return self.httpclient.request(url='orders', method='post', data=data)
+        return self.http.request(url='orders', method='post', data=data)
+
+    def get_order(self, uuid_number):
+        return self.http.request(url='orders/' + uuid_number, method='get')
+
+    def delete_order(self, uuid_number):
+        return self.http.request(url='orders/', method='delete', data={'uuid_number': uuid_number})
 
 
 class HttpClient:
-    test = False
     account = ''
     secret_key = ''
     headers = {}
-    token = ''
+    time_expired = 0
+    url = 'https://api.cdek.ru/v2/'
 
     def __init__(self, account, secret_key):
+
         self.account = account
         self.secret_key = secret_key
 
-    def request(self, url, method, params=None, data=None):
+    def request(self, url, method, data=None):
         try:
-            if method == 'get':
-                response = requests.get(url=self.get_url() + url, params=data, headers=self.headers)
-            elif method == 'post':
-                if url == 'oauth/token':
-                    self.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-
-                    response = requests.post(url=self.get_url() + url, data=data, headers=self.headers)
-                    self.token = response.json()['access_token']
-                    self.headers['Authorization'] = 'Bearer ' + self.token
-                else:
-                    self.headers['Content-Type'] = 'application/json'
-                    response = requests.post(url=self.get_url() + url, json=data, headers=self.headers)
-
+            if self.is_expired():
+                self.auth()
+            if method == 'post' or method == 'patch':
+                self.headers['Content-Type'] = 'application/json'
+            response = requests.request(method=method, url=self.url + url, json=data, headers=self.headers)
             return response.json()
         except Exception as e:
             print(e)
 
-    def get_token(self):
-        self.request(url='oauth/token', method='post', data={'grant_type': 'client_credentials',
-                                                                 'client_id': self.account,
-                                                                 'client_secret': self.secret_key})
+    def auth(self):
+        try:
+            self.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+            data = requests.post(url=self.url + 'oauth/token', headers=self.headers,
+                                 data={'grant_type': 'client_credentials',
+                                       'client_id': self.account,
+                                       'client_secret': self.secret_key},
+                                 )
+            self.headers['Authorization'] = 'Bearer ' + data.json()['access_token']
+        except Exception as e:
+            print(e)
 
-    def get_url(self):
-        if self.test:
-            return 'https://api.edu.cdek.ru/v2/'
+    def is_expired(self):
+        if self.time_expired - time.time() > 0:
+            return False
         else:
-            return 'https://api.cdek.ru/v2/'
+            return True
+
+    def set_test(self, is_test):
+        if is_test:
+            self.url = 'https://api.edu.cdek.ru/v2/'
+        else:
+            self.url = 'https://api.cdek.ru/v2/'
